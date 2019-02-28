7707879be0ec51b79da4f894682871ec48568168
Gurjit Chahal <gchahal@petzent.com>
2019-02-28 14:43:14 -0800

    jsx practice with react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
+// import the react and react-dom libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//create a react component
+const App = () => {
+    return <div>
+        <label className = "label" for = "name"> Enter name: </label>
+        <input id ="name" type = "text" />
+        <button style = {{backgroundColor:'blue',  color: 'white'}}>Submit</button> 
+    </div>;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+};
+
+//take the react component and show on screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
