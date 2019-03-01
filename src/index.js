2a343cbb899b2213ba8f5f4c861c7ce13d042526
Gurjit Chahal <gchahal@petzent.com>
2019-03-01 14:29:23 -0800

    created comment components

--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
+import CommentDetail from './CommentDetail';
+
 
 const App = () => {
     return (
         <div className = "ui container comments" >
-        <div className = "comment">
-        <a href ="/" className = "avatar">
-        <img alt ="avatar" src ={faker.image.avatar()}/>
-        </a>
-        <div className = "content">
-        <a href = "/" className = "author" >
-        Sam
-        </a>
-        <div className = "metadata">
-        <span className = "date">Today at 6:00 PM</span>
-        </div>
-        <div className ="text">Nice blog post!</div>
-        </div>
-        </div>
+        <CommentDetail author = "Sam" 
+         date = "Today at 4:45PM" 
+         avatar = "faker.image.avatar"
+         content = "woah!"
+         avatar = {faker.image.avatar()}
+         />
+        <CommentDetail 
+         author = "Jake" 
+         date = "Today at 2:45PM" 
+         content = "cool post"
+         avatar = {faker.image.avatar()}
+         />
+        <CommentDetail 
+         author = "Max" 
+         date = "Today at 1:45PM"
+         content = "cool!"
+         avatar = {faker.image.avatar()}
+         />
         </div>
     );
 };
