96f5b7462fc485746f5d4a6f064a3990e17e80f4
Gurjit Chahal <gchahal@petzent.com>
2019-03-05 15:18:00 -0800

    added loading screen

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Loading from './Loading';
 
 class App extends React.Component {
 
@@ -22,7 +23,7 @@ render() {
         return <SeasonDisplay lat = {this.state.lat} />
     }
    
-        return <div>Loading...</div>
+        return <Loading message = "Please accept location request"/>;
     
     
 }
